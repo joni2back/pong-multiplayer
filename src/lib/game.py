cd8c1ba624401ca96143082bdec0542da9a3d967
Dusk Banks <me@bb010g.com>
2020-10-31 20:18:07 -0700

    update server data on every frame
    this makes secondary clients actually update on time! we're ticking now.

--- src/lib/game.py
+++ src/lib/game.py
@@ -17,7 +17,9 @@ def connect():
         sys.exit(1)
 
 class Game(pyglet.window.Window):
+    server_data = None
     running = False
+    sleep_idle = True
     racket_left = None
     racket_right = None
     racket_me = None
@@ -30,10 +32,12 @@ class Game(pyglet.window.Window):
         self.multiplayer_mode = multiplayer_mode
         if self.multiplayer_mode:
             self.me, self.conn = connect()
+            clock.schedule(self.update_server_data)
 
     def draw(self):
+        sleep_time = clock.get_sleep_time(self.sleep_idle)
         self.debug_text.text = \
-            f"FPS: {clock.get_fps()}, SleepTime: {clock.get_sleep_time(True)}"
+            f"FPS: {clock.get_fps()}, SleepTime: {sleep_time}"
         if self.multiplayer_mode:
             self.draw_multiplayer()
         else:
@@ -41,16 +45,18 @@ class Game(pyglet.window.Window):
 
     def run(self):
         if not self.running:
-            clock.schedule_interval(self.ball.moving, .005)
+            if self.primary_client:
+                clock.schedule_interval(self.ball.moving, .005)
             self.running = True
 
     def pause(self):
         if self.running:
-            clock.unschedule(self.ball.moving)
+            if self.primary_client:
+                clock.unschedule(self.ball.moving)
             self.running = False
 
     def load_sprites(self):
-        self.debug_text = pyglet.text.Label("FPS: ?, SleepTime: ?", font_size=10, x=0, y=settings.WINDOW_HEIGHT, anchor_x='left', anchor_y='top')
+        self.debug_text = pyglet.text.Label('Debug!', font_size=10, x=0, y=settings.WINDOW_HEIGHT, anchor_x='left', anchor_y='top')
         self.score = pyglet.text.Label('', font_size=15, x=settings.WINDOW_WIDTH/2, y=settings.WINDOW_HEIGHT - 15, anchor_x='center', anchor_y='center')
         self.racket_left = Racket(pyglet.resource.image(settings.RACKET_IMG)).center_anchor_y(settings.WINDOW_HEIGHT)
         self.racket_right = Racket(pyglet.resource.image(settings.RACKET_IMG)).center_anchor_y(settings.WINDOW_HEIGHT)
@@ -78,7 +84,7 @@ class Game(pyglet.window.Window):
         if self.ball.check_collision_laterals(settings.WINDOW_HEIGHT):
             self.ball.hit_lateral()
 
-    def update_server_data(self):
+    def update_server_data(self, dt):
         data = {
             "ball": {
                 "x": self.ball.x,
@@ -90,33 +96,31 @@ class Game(pyglet.window.Window):
             }
         }
         self.conn.send(simplejson.dumps(data).encode('utf-8'))
-        return simplejson.loads(self.conn.recv(2000).decode('utf-8'))
+        self.server_data = simplejson.loads(self.conn.recv(2000).decode('utf-8'))
+        return self.server_data
 
     def update_multiplayer_positions(self, data):
         for playerid in data.keys():
-            try:
-                if playerid != self.me:
-                    self.racket_vs.y = data[playerid]['racket']['y']
-                    if not self.primary_client:
-                        self.ball.x = data[playerid]['ball']['x']
-                        self.ball.y = data[playerid]['ball']['y']
-            except:
-                pass
+            if playerid != self.me:
+                player_data = data[playerid]
+                if 'racket' in player_data:
+                    self.racket_vs.y = player_data['racket']['y']
+                if not self.primary_client and 'ball' in player_data:
+                    self.ball.x = player_data['ball']['x']
+                    self.ball.y = player_data['ball']['y']
 
     def draw_multiplayer(self):
-        data = self.update_server_data()
-        self.define_players(data)
+        self.define_players(self.server_data)
 
-        if self.primary_client:
-            if len(data.keys()) == 2:
-                self.run()
-            else:
-                self.pause()
+        if len(self.server_data.keys()) == 2:
+            self.run()
+        else:
+            self.pause()
 
         if self.primary_client:
             self.on_collision()
 
-        self.update_multiplayer_positions(data)
+        self.update_multiplayer_positions(self.server_data)
 
     def draw_singleplayer(self):
         self.run()
